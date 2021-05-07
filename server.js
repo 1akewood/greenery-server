3b37b87d73ada590ec32f3d14b55ab1771fc22e1
sangwoo98 <sangwoo98.kang@gmail.com>
2021-05-07 09:44:12 +0900

    bodyparser error change

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParaser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/process/login', function(req, res) {  
     var paramId = req.param('id');
